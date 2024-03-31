fec97a8f349e220a878e259b48eb9da46c2d440e
SHIM <mapat717@gmail.com>
2024-03-31 22:45:31 +0300

    first commit

---
+++ source/GOLive/lab_1/GOLive.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace lab_1
+{
+    partial class Screen
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.BluBut = new System.Windows.Forms.Button();
+            this.RedBut = new System.Windows.Forms.Button();
+            this.Continue = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Race = new System.Windows.Forms.Label();
+            this.MapIsLife = new System.Windows.Forms.DataGridView();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MapIsLife)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.IsSplitterFixed = true;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.SplitContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.AutoScroll = true;
+            this.SplitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.SplitContainer.Panel1.Controls.Add(this.BluBut);
+            this.SplitContainer.Panel1.Controls.Add(this.RedBut);
+            this.SplitContainer.Panel1.Controls.Add(this.Continue);
+            this.SplitContainer.Panel1.Controls.Add(this.Stop);
+            this.SplitContainer.Panel1.Controls.Add(this.Race);
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.AllowDrop = true;
+            this.SplitContainer.Panel2.Controls.Add(this.MapIsLife);
+            this.SplitContainer.Size = new System.Drawing.Size(1045, 213);
+            this.SplitContainer.SplitterDistance = 193;
+            this.SplitContainer.SplitterWidth = 5;
+            this.SplitContainer.TabIndex = 1;
+            // 
+            // BluBut
+            // 
+            this.BluBut.BackColor = System.Drawing.Color.Blue;
+            this.BluBut.Location = new System.Drawing.Point(13, 148);
+            this.BluBut.Margin = new System.Windows.Forms.Padding(4);
+            this.BluBut.Name = "BluBut";
+            this.BluBut.Size = new System.Drawing.Size(85, 53);
+            this.BluBut.TabIndex = 8;
+            this.BluBut.Text = "Синий";
+            this.BluBut.UseVisualStyleBackColor = false;
+            this.BluBut.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // RedBut
+            // 
+            this.RedBut.BackColor = System.Drawing.Color.Red;
+            this.RedBut.Location = new System.Drawing.Point(101, 148);
+            this.RedBut.Margin = new System.Windows.Forms.Padding(4);
+            this.RedBut.Name = "RedBut";
+            this.RedBut.Size = new System.Drawing.Size(90, 53);
+            this.RedBut.TabIndex = 7;
+            this.RedBut.Text = "Красный";
+            this.RedBut.UseVisualStyleBackColor = false;
+            this.RedBut.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Continue
+            // 
+            this.Continue.BackColor = System.Drawing.Color.Lime;
+            this.Continue.Location = new System.Drawing.Point(13, 99);
+            this.Continue.Margin = new System.Windows.Forms.Padding(4);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(174, 41);
+            this.Continue.TabIndex = 6;
+            this.Continue.Text = "Продолжить";
+            this.Continue.UseVisualStyleBackColor = false;
+            this.Continue.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Red;
+            this.Stop.Location = new System.Drawing.Point(13, 13);
+            this.Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(174, 42);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Cтоп";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Race
+            // 
+            this.Race.AutoSize = true;
+            this.Race.Location = new System.Drawing.Point(66, 68);
+            this.Race.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Race.Name = "Race";
+            this.Race.Size = new System.Drawing.Size(66, 17);
+            this.Race.TabIndex = 4;
+            this.Race.Text = "Время: 0";
+            // 
+            // MapIsLife
+            // 
+            this.MapIsLife.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MapIsLife.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MapIsLife.Location = new System.Drawing.Point(0, 0);
+            this.MapIsLife.Margin = new System.Windows.Forms.Padding(4);
+            this.MapIsLife.Name = "MapIsLife";
+            this.MapIsLife.RowHeadersWidth = 51;
+            this.MapIsLife.Size = new System.Drawing.Size(847, 213);
+            this.MapIsLife.TabIndex = 0;
+            this.MapIsLife.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PoleLife_CellClick);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 213);
+            this.Controls.Add(this.SplitContainer);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Screen";
+            this.Text = "Form1";
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel1.PerformLayout();
+            this.SplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MapIsLife)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.DataGridView MapIsLife;
+        private System.Windows.Forms.Label Race;
+        private System.Windows.Forms.Button Continue;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button BluBut;
+        private System.Windows.Forms.Button RedBut;
+    }
+}
+
---
+++ source/GOLive/lab_1/GOLive.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_1
+{
+    public partial class Screen : Form
+    {
+        int[,] redbox; //двумерный массив, хранящий живые красные клетки
+        int[,] tworedbox; //двумерный массив, в который заносятся новые поколения красных клеток
+        int time = 1;
+        int[,] bluebox; //двумерный массив, хранящий живые синие клетки
+        int[,] twobluebox; //двумерный массив, в который заносятся новые поколения синих клеток
+        bool SelCol = false; //переменная отвечающая какой цвет будет ставить пользователь
+        //false - красный
+        //true - синий
+        int Box_map = 30;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = false; //таймер выключается
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = true; //таймер выключается
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SelCol = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SelCol = true;
+        }
+
+        //функция размножения красных клеток
+        void FunctionLifeIsRed()
+        {
+            //переменные определяющие индекс клеток с левой, правой, сверху, снизу
+            int Left, Right, Up, Down, Live; //Live определяет количество живых клеток рядом с клеткой
+            for (int k = 0; k < Box_map; k++)      
+                for (int g = 0; g < Box_map; g++)
+                { 
+                    if (k != 0) //если не первый столбец в поле
+                        Left = k - 1;
+                    else
+                        Left = Box_map - 1; //то присваивается самый парвый столбец
+                    if (k != Box_map - 1) //если не последний столбец в поле
+                        Right = k + 1;
+                    else
+                        Right = 0;
+                    if (g != 0) //если не верхняя строка в поле
+                        Up = g - 1;
+                    else
+                        Up = Box_map - 1;
+                    if (g != Box_map - 1) //если не нижняя строка в поле
+                        Down = g + 1;
+                    else
+                        Down = 0;
+                    Live = 0;
+                    //подсчет живых клеток вокруг клетки
+                    Live += redbox[k, Up];
+                    Live += redbox[k, Down];
+                    Live += redbox[Left, g];
+                    Live += redbox[Right, g];
+                    Live += redbox[Left, Up];
+                    Live += redbox[Left, Down];
+                    Live += redbox[Right, Up];
+                    Live += redbox[Right, Down];
+                    //если вокруг три живых клетки и клетка мертва
+                    if (Live == 3 && redbox[k, g] == 0)  
+                        tworedbox[k, g] = 1; //клетка становится живой
+                    //если вокруг меньше двух или больше трех клеток и клетка живая
+                    else if ((Live < 2 || Live > 3) && (redbox[k, g] == 1))
+                        tworedbox[k, g] = 0; //клетка становится мертвой
+                    else
+                        tworedbox[k, g] = redbox[k, g]; //иначе клетка остается без изменений
+                }
+        }
+
+        //функция приравнивания элементов массива нынешнего поколения красных клеток к следующему
+        void NewForOldRed()
+        {
+            for (int k = 0; k < Box_map; k++)
+                for (int g = 0; g < Box_map; g++)
+                    redbox[k, g] = tworedbox[k, g];
+        }
+
+        //функция размножения синих клеток с таким же алгоритмом, как и у красных
+        void FunctionLifeIsBlue()
+        {
+            int Left, Right, Up, Down, Live;
+            for (int i = 0; i < Box_map; i++)
+                for (int j = 0; j < Box_map; j++)
+                {
+                    if (i != 0)
+                        Left = i - 1;
+                    else
+                        Left = Box_map - 1;
+                    if (i != Box_map - 1)
+                        Right = i + 1;
+                    else
+                        Right = 0;
+                    if (j != 0)
+                        Up = j - 1;
+                    else
+                        Up = Box_map - 1;
+                    if (j != Box_map - 1)
+                        Down = j + 1;
+                    else
+                        Down = 0;
+                    Live = 0;
+                    Live += bluebox[i, Up];
+                    Live += bluebox[i, Down];
+                    Live += bluebox[Left, j];
+                    Live += bluebox[Right, j];
+                    Live += bluebox[Left, Up];
+                    Live += bluebox[Left, Down];
+                    Live += bluebox[Right, Up];
+                    Live += bluebox[Right, Down];
+                    if (Live == 3 && bluebox[i, j] == 0)
+                        twobluebox[i, j] = 1;
+                    else if ((Live < 2 || Live > 3) && (bluebox[i, j] == 1))
+                        twobluebox[i, j] = 0;
+                    else
+                        twobluebox[i, j] = bluebox[i, j];
+                }
+        }
+
+        //функция приравнивания элементов массива нынешнего поколения синих клеток к следующему
+        void NewForOldBlue()
+        {
+            for (int i = 0; i < Box_map; i++)
+                for (int j = 0; j < Box_map; j++)
+                    bluebox[i, j] = twobluebox[i, j];
+        }
+
+        //отображение красных клеток
+        void SeeRedToMap()
+        {
+            for (int i = 0; i < Box_map; i++)
+                for (int j = 0; j < Box_map; j++)
+                {
+                    //если в данной ячейке поля красная клетка жива и синяя мертва
+                    if (redbox[i, j] == 1 && bluebox[i, j] == 0) 
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Red; //ячейка красная
+                    //если в ячейке красная мертва и синяя живы
+                    else if (redbox[i, j] == 0 && bluebox[i, j] == 1)
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Blue; //ячейка синяя
+                    //если в ячейке красная и синяя жива
+                    else if (redbox[i, j] == 1 && bluebox[i, j] == 1) 
+                    {
+                        bluebox[i, j] = 0; //синяя клетка умирает
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Red; //ячейка красная
+                    }
+                    else
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.White; //иначе ячейка белая
+
+                }
+            MapIsLife.Refresh();
+        }
+
+        //отображение синих клеток
+        void SeeBlueToMap()
+        {
+            for (int i = 0; i < Box_map; i++)
+                for (int j = 0; j < Box_map; j++)
+                {
+                    //если в данной ячейке поля синяя жива и красная мертва
+                    if (bluebox[i, j] == 1 && redbox[i, j] == 0)
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Blue; //ячейка синяя
+                    //если синяя клетка мертва и красная жива
+                    else if(bluebox[i, j] == 0 && redbox[i, j] == 1)
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Red; //ячейка красная
+                    //если красная и синяя живы
+                    else if (redbox[i, j] == 1 && bluebox[i, j] == 1)
+                    {
+                        redbox[i, j] = 0; //красная клетка умирает
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.Blue; //ячейка синяя
+                    }
+                    else
+                        MapIsLife[j, i].Style.BackColor = System.Drawing.Color.White; //иначе ячейка белая
+
+                }
+        }
+
+        public Screen()
+        {
+            InitializeComponent();
+            redbox = new int[Box_map, Box_map]; //определение размера массива 
+            bluebox = new int[Box_map, Box_map]; //определение размера массива
+            tworedbox = new int[Box_map, Box_map]; //определение размера массива
+            twobluebox = new int[Box_map, Box_map]; //определение размера массива
+            MapIsLife.ColumnCount = Box_map; //количество столбцов поля
+            MapIsLife.RowCount = Box_map; //количество строк в поле
+            MapIsLife.ReadOnly = true; //запрет на изменение данных в ячейках 
+            MapIsLife.AllowUserToDeleteRows = false; //запрещает пользователю удалять строки
+            MapIsLife.RowHeadersVisible = false; //убирает столбец показыающий, что происходит со строками
+            MapIsLife.AllowUserToResizeColumns = false; //запрещает пользователю менять размер столбцов
+            MapIsLife.AllowUserToResizeRows = false;//запрещает пользователю менять размер полей
+            for (int i = 0; i < Box_map; i++)
+            {
+                MapIsLife.Columns[i].Width = 20; //ширина ячеек в поле
+                MapIsLife.Rows[i].Height = 20; //высота ячеек в поле
+                MapIsLife.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable; //отключает сортировку столбца
+                for (int j = 0; j < Box_map; j++) //приравнивание всех масивов к 0
+                {
+                    redbox[i, j] = 0;
+                    bluebox[i, j] = 0;
+                    tworedbox[i, j] = 0;
+                    twobluebox[i, j] = 0;
+                }
+            }
+            MapIsLife.ColumnHeadersVisible = false; //удаление заголовков
+        }
+
+        private void PoleLife_CellClick(object sender, DataGridViewCellEventArgs e) //событие клик по ячейке
+        {
+            if (SelCol == false) //если в Color текст
+            {
+                if (MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor != System.Drawing.Color.Red) //если ячейка не красная
+                {
+                    MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor = System.Drawing.Color.Red; //она становится красной
+                    redbox[e.RowIndex, e.ColumnIndex] = 1; //ячейка в массиве с красными клетками становится живой
+                    bluebox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с синими клетками становится мёртвой
+                }
+                else
+                {
+                    MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor = System.Drawing.Color.White; //она становится белой
+                    redbox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с красными клетками становится мёртвой
+                    bluebox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с синими клетками становится мёртвой
+                }
+            }
+            else if (SelCol == true) //если в Color текст
+            {
+                if (MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor != System.Drawing.Color.Blue) //если ячейка не синяя
+                {
+                    MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor = System.Drawing.Color.Blue; //она становится синей
+                    redbox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с красными клетками становится мёртвой
+                    bluebox[e.RowIndex, e.ColumnIndex] = 1; //ячейка в массиве с синими клетками становится живой
+                }
+                else
+                {
+                    MapIsLife[e.ColumnIndex, e.RowIndex].Style.BackColor = System.Drawing.Color.White; //она становится белой
+                    redbox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с красными клетками становится мёртвой
+                    bluebox[e.RowIndex, e.ColumnIndex] = 0; //ячейка в массиве с синими клетками становится мёртвой
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) //Что происходит каждую секунду
+        {
+            time += 1;
+            Race.Text = "Время: " + Convert.ToString(time);
+            FunctionLifeIsRed();
+            NewForOldRed();
+            FunctionLifeIsBlue();
+            NewForOldBlue();
+            SeeRedToMap();
+            SeeBlueToMap();
+        }
+    }
+}
